a950c5f17d71c41f7f3a3d6c7d9391aad638836a
Tolieck <tolieck89@gmail.com>
2025-02-02 00:47:50 +0200

    26 Lection

---
+++ webShop/JS/app.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    showCategories();
+
+    document.querySelector('.categories').addEventListener('click', (event) => {
+        if (
+        event.target.tagName === "DIV" && 
+        event.target.classList.contains("category")){
+            const categoryID = event.target.getAttribute('data-id');
+            showProductsByCategory(categoryID);
+        }
+    });
+
+    document.querySelector('.products').addEventListener('click', (event) => {
+        if (
+            event.target.tagName === "DIV" && 
+            event.target.classList.contains("product")){
+                console.dir(event.target.textContent);
+            }
+});
+});
+
+
+function showCategories(){
+   
+    showEntities('.categories', 'category', categories);
+
+}
+
+function showProductsByCategory(categoryID){
+
+    console.log(`U wanna get products by category ${categoryID}`);
+    
+    const myCategory = categories.find(category => category.id === categoryID);
+    const products = myCategory.items;
+    showEntities('.products', 'product', products);
+}
+
+
+function showEntities(parentSelector, elementClassName, entities) {
+
+    const parentElement = document.querySelector(parentSelector);
+    parentElement.innerHTML = '';
+
+  
+    for (let entity  of entities) {
+    const divElement = document.createElement('div');
+    divElement.textContent = entity.name;
+    divElement.classList.add(elementClassName);
+    divElement.setAttribute('data-id', entity.id);
+    parentElement.appendChild(divElement);
+}}
---
+++ webShop/JS/shopdata.js
@@ -0,0 +1,24 @@
+const categories = [
+    {
+        id: "phones",
+        name: "Телефони",
+        items: [
+            {id: "1", name: 'Iphone', price: 2000, description: 'This is iPhone'},
+            {id: "2", name: 'Galaxy', price: 1300, description: 'This is Galaxy'},
+            {id: "3", name: 'Huawei', price: 1200, description: 'This is Huawei'},
+            {id: "4", name: 'OnePlus', price: 1000, description: 'This is OnePlus'},
+        ]
+    }, 
+    {
+        id: "laptops",
+        name: "Ноутбуки",
+        items: [
+            {id: "1", name: 'Mackbook', price: 3000, description: 'This is Mackbook'},
+            {id: "2", name: 'Asus Zenbook', price: 2500, description: 'This is Zenbook'},
+            {id: "3", name: 'HP XXX', price: 1200, description: 'This is XXX'},
+            {id: "4", name: 'Dell', price: 1000, description: 'This is Dell'},
+            {id: "5", name: 'Lenovo', price: 1000, description: 'This is Lenovo'},
+        ]
+    }, 
+    
+]
